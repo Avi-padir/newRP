e7ef94a2291220c626a622b9b3ddfbaa5f1346b0
Avi-padir <avishkarpadir2000@gmail.com>
2025-05-21 18:15:46 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -6,6 +6,6 @@ sudo rm -rf /usr/share/nginx/html/*
 echo "Copying new build files to Nginx directory..."
 sudo cp -r /home/ec2-user/nodejsbuild/build/* /usr/share/nginx/html/
 
-echo "Restarting Nginx..."
+echo "Restarting Nginx.."
 sudo systemctl start nginx
 sudo systemctl enable nginx
